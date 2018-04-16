bb711f2e36b4bbd27f1ddbb0039f13838ff8f11b
ggallici <gallicig94@gmail.com>
2018-04-16 09:58:12 -0300

    Primeras clases y estructura basica del proyecto

---
+++ src/test/java/dominio/ClienteTest.java
@@ -0,0 +1,19 @@
+package dominio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClienteTest {
+
+	@Before
+	public void setUp() throws Exception {
+	
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
