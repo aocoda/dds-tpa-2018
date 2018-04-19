a4a5d75f9a1a388e797326c4743acdf1c1aaba7d
GuidoMartinez <guidojmartinez@gmail.com>
2018-04-19 20:39:06 -0300

    Se agrega comportamiento inicial a la clase Cliente

--- src/main/java/dominio/Cliente.java
+++ src/main/java/dominio/Cliente.java
@@ -1,5 +1,65 @@
 package dominio;
 
-public class Cliente {
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashSet;
 
+public class Cliente {
+	
+	private String nombreCompleto;
+	private String apellido;
+	private Documento documento;
+	private String telefono;
+	private String domicilio;
+	private LocalDate fechaAltaServicio;
+	//private Categoria categoria;
+	private Collection<Dispositivo> dispositivos = new HashSet<Dispositivo>();
+	
+	
+	public Cliente(String nombre, Documento documento, String telefono, String domicilio,
+			LocalDate fechaAltaServicio, Collection<Dispositivo> dispositivos) {
+		
+		this.nombreCompleto = nombre;
+		this.documento = documento;
+		this.telefono = telefono;
+		this.domicilio = domicilio;
+		this.fechaAltaServicio = fechaAltaServicio;
+		this.dispositivos = dispositivos;
+	}
+
+	public boolean existeDispositivoEncendido() {
+		
+		return dispositivos
+				.stream()
+				.anyMatch(dispositivo -> dispositivo.estaEncendido());
+	}
+	
+	public long cantidadDispositivosEncendidos() {
+		
+		return dispositivos
+				.stream()
+				.filter(dispositivo -> dispositivo.estaEncendido())
+				.count();
+	}
+	
+	public long cantidadDispositivosApagados() {
+		
+		return dispositivos
+				.stream()
+				.filter(dispositivo -> !dispositivo.estaEncendido())
+				.count();
+	}
+	
+	public long cantidadDispositivos() {
+		
+		return dispositivos.size();
+	}
+	
+	public double consumoMensual() {
+		
+		return dispositivos
+				.stream()
+				.reduce(0d, (a,d) -> a + d.getConsumoPorMes(), Double::sum);
+		
+	}
 }
