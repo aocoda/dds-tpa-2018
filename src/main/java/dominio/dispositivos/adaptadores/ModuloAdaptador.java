c6584f0da858283720ed932c49d01125273b8159
ggallici <gallicig94@gmail.com>
2018-05-16 17:14:30 -0300

    indento

--- src/main/java/dominio/dispositivos/adaptadores/ModuloAdaptador.java
+++ src/main/java/dominio/dispositivos/adaptadores/ModuloAdaptador.java
@@ -3,5 +3,6 @@ package dominio.dispositivos.adaptadores;
 import dominio.dispositivos.*;
 
 public interface ModuloAdaptador {
+	
 	public DispositivoInteligente adaptar(DispositivoEstandar dispositivo);
 }
