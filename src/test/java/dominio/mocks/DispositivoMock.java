170cf66f26a9c6d7de9e42274e420171957d54c5
ggallici <gallicig94@gmail.com>
2018-05-18 11:25:35 +0200

    subo clase para mockear dispositivo inteligente

---
+++ src/test/java/dominio/mocks/DispositivoMock.java
@@ -0,0 +1,11 @@
+package dominio.mocks;
+
+import dominio.dispositivos.DispositivoInteligente;
+
+public class DispositivoMock extends DispositivoInteligente {
+
+	public DispositivoMock(String nombreGenerico, double consumoPorHora) {
+		
+		super(nombreGenerico, consumoPorHora);
+	}
+}
