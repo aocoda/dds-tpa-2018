a2f5dba2d7f46ca9f22efb464c3c8621f59ffe00
ggallici <gallicig94@gmail.com>
2018-05-17 21:12:25 -0300

    indentado

--- src/main/java/dominio/dispositivos/inteligentes/PeriodoUtils.java
+++ src/main/java/dominio/dispositivos/inteligentes/PeriodoUtils.java
@@ -5,10 +5,12 @@ import java.time.LocalDateTime;
 public interface PeriodoUtils {
 
 	default public LocalDateTime maximo(LocalDateTime unaFecha, LocalDateTime otraFecha) {
+		
 		return unaFecha.isAfter(otraFecha) ? unaFecha : otraFecha;
 	}
 
 	default public LocalDateTime minimo(LocalDateTime unaFecha, LocalDateTime otraFecha) {
+		
 		return unaFecha.isBefore(otraFecha) ? unaFecha : otraFecha;
 	}
 }
