568e982167f86b3e1302a3a65b079397545de909
AndresBettinelli <bettinelli.andres@gmail.com>
2018-05-17 22:04:02 -0300

    Se agregan test de DI y Periodos

--- src/test/java/dominio/AdminTest.java
+++ src/test/java/dominio/AdminTest.java
@@ -7,16 +7,16 @@ import java.time.LocalDate;
 import org.junit.Test;
 
 public class AdminTest {
-	
+
 	private Admin admin;
-	
+
 	@Test
 	public void siUnAdministradorEntraHac4meses_ElMetodo_antiguedad_DebeArrojar4() {
-		
+
 		LocalDate fecha = LocalDate.now().minusMonths(4);
-		
+
 		admin = new Admin(null, null, fecha);
-		
+
 		assertEquals(4, admin.antiguedad());
 	}
 }
--- src/test/java/dominio/DispositivoInteligenteTest.java
+++ src/test/java/dominio/DispositivoInteligenteTest.java
@@ -2,13 +2,20 @@ package dominio;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
 import org.junit.Test;
 
-import dominio.dispositivos.DispositivoInteligente;
+import dominio.dispositivos.*;
 
 public class DispositivoInteligenteTest {
 
 	private DispositivoInteligente dispositivoTest = new DispositivoInteligente("dispositivoTest", 100);
+	private LocalDateTime dia1 = LocalDateTime.of(2018, 4, 1, 0, 0);
+	private LocalDateTime dia2 = LocalDateTime.of(2018, 4, 2, 0, 0);
+	private LocalDateTime dia3 = LocalDateTime.of(2018, 4, 3, 0, 0);
+	private LocalDateTime dia4 = LocalDateTime.of(2018, 4, 4, 0, 0);
+	private LocalDateTime dia5 = LocalDateTime.of(2018, 4, 5, 0, 0);
+	private LocalDateTime dia5_1 = LocalDateTime.of(2018, 4, 5, 1, 0);
 
 	@Test
 	public void SiUnDispositivoEstaApagado_ySeLeDieraLaOrdenDeApagarse_noDeberaHacerNadaYPermancerApagado() {
@@ -57,5 +64,70 @@ public class DispositivoInteligenteTest {
 		dispositivoTest.encender();
 
 		assertTrue(dispositivoTest.estaEncendido());
+	}
+	
+	@Test
+	public void unHistorialConUnConsumoDe24HSQueSeEncuentraTotalmenteDentroDelPeriodoAAnalizar() {
+		dispositivoTest.encender();
+		Periodo dia2a3 = new Periodo(dia2, dia3);
+		Periodo dia1a5 = new Periodo(dia1, dia5);
+		dispositivoTest.addUso(dia2a3);
+		
+		assertEquals(2400, dispositivoTest.consumoDe(dia1a5), 0);
+	}
+	
+	@Test
+	public void unHistorialCon2ConsumoDe48HSQueSeEncuentraTotalmenteDentroDelPeriodoAAnalizar() {
+		dispositivoTest.encender();
+		Periodo dia2a3 = new Periodo(dia2, dia3);
+		Periodo dia3a4 = new Periodo(dia3, dia4);
+		Periodo dia1a5 = new Periodo(dia1, dia5);
+		dispositivoTest.addUso(dia2a3);
+		dispositivoTest.addUso(dia3a4);
+		
+		assertEquals(4800, dispositivoTest.consumoDe(dia1a5), 0);
+	}
+	
+	
+	@Test
+	public void unHistorialConUnConsumoDe72HSQueSeEncuentraSolo48HsSeAnalizan_ComoParticular_TerminanLaMismaFecha() {
+		dispositivoTest.encender();
+		Periodo dia1a3 = new Periodo(dia1, dia3);
+		Periodo dia4a5 = new Periodo(dia4, dia5);
+		Periodo dia2a5 = new Periodo(dia2, dia5);
+		dispositivoTest.addUso(dia1a3);
+		dispositivoTest.addUso(dia4a5);
+		
+		assertEquals(4800, dispositivoTest.consumoDe(dia2a5), 0);
+	}
+	
+	@Test
+	public void unHistorialConUnConsumoDe72HSQueSeEncuentraSolo48HsSeAnalizan_ComoParticularUsoTerminan1HoraAntesDelPeriodo() {
+		dispositivoTest.encender();
+		Periodo dia1a3 = new Periodo(dia1, dia3);
+		Periodo dia4a5 = new Periodo(dia4, dia5);
+		Periodo dia2a5_1 = new Periodo(dia2, dia5_1);
+		dispositivoTest.addUso(dia1a3);
+		dispositivoTest.addUso(dia4a5);
+		
+		assertEquals(4800, dispositivoTest.consumoDe(dia2a5_1), 0);
+	}
+	
+	@Test
+	public void unHistorialDondeLosUsosDentroNoCaenDelPeriodoAnalizarDebeDar0() {
+		dispositivoTest.encender();
+		Periodo dia1a3 = new Periodo(dia1, dia3);
+		Periodo dia5a5_1 = new Periodo(dia5, dia5_1);
+		dispositivoTest.addUso(dia1a3);
+		
+		assertEquals(0, dispositivoTest.consumoDe(dia5a5_1), 0);
+	}
+	
+	@Test
+	public void unPeriodoDondeElDispositvoTieneUnaListaVaciaDebeDar0() {
+		dispositivoTest.encender();
+		Periodo dia5a5_1 = new Periodo(dia5, dia5_1);
+		
+		assertEquals(0, dispositivoTest.consumoDe(dia5a5_1), 0);
 	}
 }
