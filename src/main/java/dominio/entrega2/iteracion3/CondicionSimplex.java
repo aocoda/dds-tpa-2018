7d3fddd0ea7fcab0010c27490a0e986978c3c6c9
ggallici <gallicig94@gmail.com>
2018-07-05 15:36:50 +0200

    dependencias (?

--- src/main/java/dominio/entrega2/iteracion3/CondicionSimplex.java
+++ src/main/java/dominio/entrega2/iteracion3/CondicionSimplex.java
@@ -1,13 +1,30 @@
 package dominio.entrega2.iteracion3;
 
+import java.util.List;
+
+import dominio.dispositivos.DispositivoInteligente;
+import dominio.dispositivos.Periodo;
 import dominio.entrega2.iteracion2.AsesorDeUso;
 import dominio.reglas.condiciones.Condicion;
 
 public class CondicionSimplex implements Condicion {
 
+	private AsesorDeUso asesor;
+	private DispositivoInteligente dispositivo;
+	private List<DispositivoInteligente> dispositivos;
+	private Periodo periodo;
+
+	public CondicionSimplex(AsesorDeUso asesor, DispositivoInteligente dispositivo, List<DispositivoInteligente> dispositivos, Periodo periodo) {
+		
+		this.asesor = asesor;
+		this.dispositivo = dispositivo;
+		this.dispositivos = dispositivos;
+		this.periodo = periodo;
+	}
+
 	@Override
 	public boolean seCumple() {
 		
-		return new AsesorDeUso(restricciones).superaHorasOptimas(dispositivo, dispositivos, unPeriodo);
+		return asesor.superaHorasOptimas(dispositivo, dispositivos, periodo);
 	}
 }
