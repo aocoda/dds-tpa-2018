92f6ef1801752f7365df40a5894401c8958bc9ab
ggallici <gallicig94@gmail.com>
2018-07-17 13:08:00 +0200

    arreglo bug en un test

--- src/test/java/dominio/PeriodosTest.java
+++ src/test/java/dominio/PeriodosTest.java
@@ -18,20 +18,23 @@ public class PeriodosTest {
 
 	@Test
 	public void enUnPeriodoDe25HS_ElmetodocantidadDeHorasDebeTirar25HS() {
+		
 		Periodo dia4a5_1 = new Periodo(dia4, dia5_1);
 
 		assertEquals(25, dia4a5_1.cantidadDeHoras(), 0);
 	}
 
 	@Test
-	public void enUnPeriodoDe96HS_ElmetodocantidadDeDiasDebeTirar4HS() {
+	public void enUnPeriodoDe96HS_ElmetodocantidadDeDiasDebeTirar4() {
+		
 		Periodo dia1a5 = new Periodo(dia1, dia5);
 
-		assertEquals(96, dia1a5.cantidadDeHoras(), 0);
+		assertEquals(4, dia1a5.cantidadDeDias(), 0);
 	}
 
 	@Test
 	public void si2PeriodosCompartenUnPeriodoDeTiempo_DebenContenerseMutuamente() {
+		
 		Periodo dia1a5 = new Periodo(dia1, dia5);
 		Periodo dia2a5_1 = new Periodo(dia2, dia5_1);
 
@@ -41,6 +44,7 @@ public class PeriodosTest {
 
 	@Test
 	public void unPeriodoDebeContenerseASiMismo() {
+		
 		Periodo dia1a5 = new Periodo(dia1, dia5);
 
 		assertTrue(dia1a5.contiene(dia1a5));
