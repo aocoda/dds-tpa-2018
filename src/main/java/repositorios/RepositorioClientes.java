9a18414485e17b1b7031914fca1929f9996927b2
ggallici <gallicig94@gmail.com>
2018-09-11 14:17:45 +0200

    Elimino metodo deprecado

--- src/main/java/repositorios/RepositorioClientes.java
+++ src/main/java/repositorios/RepositorioClientes.java
@@ -1,7 +1,5 @@
 package repositorios;
 
-import java.util.Optional;
-
 import dominio.Cliente;
 import dominio.dispositivos.Periodo;
 
@@ -10,14 +8,6 @@ public class RepositorioClientes extends RepositorioGenerico<Cliente> {
 	public void ejecutarReglasQueCorrespondan() {
 		
 		elementos.forEach(cliente -> cliente.ejecutarReglasQueCorrespondan());
-	}
-
-	public Optional<Cliente> getPorNumeroDocumento(int numeroDocumento) {
-		
-		return elementos
-				.stream()
-				.filter(cliente -> cliente.getNumeroDocumento() == numeroDocumento)
-				.findFirst();
 	}
 	
 	public void ejecutarApagadoAutomaticoPorConsumo(Periodo unPeriodo) {
